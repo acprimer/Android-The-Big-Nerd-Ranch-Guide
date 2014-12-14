1f58ac97033bc4e96828b1903cdab138c735d001
acprimer <729101421@qq.cm>
2014-12-14 19:44:21 +0800

    Android-The-Big-Nerd-Ranch-Guide

---
+++ app/src/androidTest/java/cn/edu/buaa/yaodh/android_the_big_nerd_ranch_guide/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cn.edu.buaa.yaodh.android_the_big_nerd_ranch_guide;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
