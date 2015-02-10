499fe00c551ca473c20ff170a9ab72c201afcd80
Dahai Yao <729101421@qq.com>
2015-02-10 21:10:47 +0800

    chap20

---
+++ app/src/main/java/cn/edu/buaa/yaodh/android_the_big_nerd_ranch_guide/criminal_intent/ImageFragment.java
@@ -0,0 +1,45 @@
+package cn.edu.buaa.yaodh.android_the_big_nerd_ranch_guide.criminal_intent;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by yaodh on 2015/2/10.
+ */
+public class ImageFragment extends DialogFragment {
+    public static final String EXTRA_IMAGE_PATH = "image_path";
+
+    public static ImageFragment newInstance(String imagePath) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_IMAGE_PATH, imagePath);
+
+        ImageFragment fragment = new ImageFragment();
+        fragment.setArguments(args);
+        fragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+
+        return fragment;
+    }
+
+    private ImageView mImageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mImageView = new ImageView(getActivity());
+        String path = (String)getArguments().getSerializable(EXTRA_IMAGE_PATH);
+        BitmapDrawable image = PictureUtils.getScaledDrawable(getActivity(), path);
+        mImageView.setImageDrawable(image);
+        return mImageView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        PictureUtils.cleanImageView(mImageView);
+    }
+}
